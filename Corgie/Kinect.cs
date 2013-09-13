5286932c26311efb624110b6c9ce250be2f897f6
Paul Demchuk <pademchuk@gmail.com>
2013-09-13 10:54:15 -0700

    first

---
+++ Corgie/Kinect.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corgie
+{
+    public class Kinect
+    {
+
+        KinectSensor Sensor;
+        Skeleton[] _skeletonData;
+
+        Skeleton _skeleton;
+
+        Skeleton PlayerSkeleton
+        {
+            get
+            {
+                if (Length(_skeleton.Position) > 0)
+                    return _skeleton;
+                else
+                    return null;
+            }
+
+            set
+            {
+                _skeleton = value;
+            }
+        }
+
+
+
+
+
+        public Kinect()
+        {
+            
+        }
+
+
+
+
+
+        public void Init()
+        {
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    Sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (Sensor != null)
+            {
+                Sensor.SkeletonStream.Enable();
+                Sensor.SkeletonFrameReady += Sensor_SkeletonFrameReady;
+                Sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+
+                _skeleton = new Skeleton();
+
+                try
+                {
+                    Sensor.Start();
+                }
+                catch (Exception e)
+                {
+                    Sensor = null;
+                }
+            }
+
+
+        }
+
+
+
+
+
+        private void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            _skeletonData = new Skeleton[0];
+            using (SkeletonFrame skelFrame = e.OpenSkeletonFrame())
+            {
+                if (skelFrame != null)
+                {
+                    _skeletonData = new Skeleton[skelFrame.SkeletonArrayLength];
+                    skelFrame.CopySkeletonDataTo(_skeletonData);
+                }
+            }
+
+            foreach (Skeleton s in _skeletonData)
+            {
+                if (_skeleton == null && (Length(s.Position) > 0))
+                {
+                    _skeleton = s;
+                }
+                else if (_skeleton != null && s.TrackingId != _skeleton.TrackingId)
+                {
+                    _skeleton = s;
+                }
+            }
+
+        }
+
+
+
+
+
+
+        private float Length(float x, float y, float z)
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        private float Length(SkeletonPoint p)
+        {
+            return (float)Math.Sqrt(p.X * p.X + p.Y * p.Y + p.Z * p.Z);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Corgie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Kinect KinectData = new Kinect();
+        DrawingGroup drawingGroup;
+        DrawingImage imageSource;
+        
+        
+
+        public MainWindow()
+        {
+            KinectData.Init();
+                
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            // Create the drawing group we'll use for drawing
+            drawingGroup = new DrawingGroup();
+
+            // Create an image source that we can use in our image control
+            imageSource = new DrawingImage(drawingGroup);
+
+            // Display the drawing using our image control
+            Image.Source = imageSource;
+        }
+    }
+}
